a8d27155006c4370c2655d81a9a55215afe8d411
fokrule <fokrule.hasan"sjinnovation.com>
2019-07-11 23:15:19 +0600

    Done adding note and getting note from mongodb

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,17 +1,33 @@
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var urlencodeParser = bodyParser.urlencoded({extended: false});
-var t = mongoose.connect('mongodb+srv://fokrule:fokrule@cluster0-kgefs.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://fokrule:fokrule@cluster0-kgefs.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
+var todoSchema = new mongoose.Schema({
+	item: String
+}); 
 
+var Todo = mongoose.model('Todo', todoSchema);
+
+/*var itemOne = Todo({item:'testing note'}).save(function(err) {
+	if (err) throw err;
+	console.log('item saved');
+})*/
 
 module.exports = function(app){
     app.get('/',function(req, res){
-        console.log('dd');
-        res.render('home');
+        Todo.find({},function(err,data){
+        	if (err) throw err;
+        	console.log(data);
+        	res.render('home',{notes: data});
+        });
+        //.render('home');
     });
 
     app.post('/', urlencodeParser, function(req, res){
-        console.log(req.body);
-        res.render('home', {data : req.body});
+    	console.log(req.body);
+        var newTodo = Todo(req.body).save(function(err, data){
+        	if (err) throw err;
+        	res.json(data);
+        });
     });
 }
