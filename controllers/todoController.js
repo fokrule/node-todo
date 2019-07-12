7db50c67819d61f76bd652a1eee18baa316a98bf
Fokrul <fokrule.hasan@sjinnovation.com>
2019-07-12 18:05:52 +0600

    Done create and add delete note

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -8,26 +8,30 @@ var todoSchema = new mongoose.Schema({
 
 var Todo = mongoose.model('Todo', todoSchema);
 
-/*var itemOne = Todo({item:'testing note'}).save(function(err) {
-	if (err) throw err;
-	console.log('item saved');
-})*/
+
 
 module.exports = function(app){
     app.get('/',function(req, res){
         Todo.find({},function(err,data){
         	if (err) throw err;
-        	console.log(data);
+            
         	res.render('home',{notes: data});
         });
         //.render('home');
     });
 
     app.post('/', urlencodeParser, function(req, res){
-    	console.log(req.body);
         var newTodo = Todo(req.body).save(function(err, data){
         	if (err) throw err;
         	res.json(data);
+        });
+    });
+
+    app.delete('/:id', function(req, res){
+      
+        Todo.find({_id: req.params.id}).deleteOne(function(err, data){
+            if (err) throw err;
+            res.json(data);
         });
     });
 }
